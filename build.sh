7f56e72f75d2437497d747f5b38f9959437430bb
Daniel Polito <danielbpolito@gmail.com>
2020-08-27 15:24:14 -0300

    Tons of Improvements

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-docker build --pull -t kooldev/wordpress:7.4-fpm-alpine 7.4-fpm-alpine
-docker build --pull -t kooldev/wordpress:7.3-fpm-alpine 7.3-fpm-alpine
-docker build --pull -t kooldev/wordpress:7.2-fpm-alpine 7.2-fpm-alpine
-docker build --pull -t kooldev/wordpress:7.1-fpm-alpine 7.1-fpm-alpine
+docker build --pull -t kooldev/wordpress:7.1 7.1
+docker build -t kooldev/wordpress:7.1-nginx 7.1-nginx
+docker build --pull -t kooldev/wordpress:7.2 7.2
+docker build -t kooldev/wordpress:7.2-nginx 7.2-nginx
+docker build --pull -t kooldev/wordpress:7.3 7.3
+docker build -t kooldev/wordpress:7.3-nginx 7.3-nginx
+docker build --pull -t kooldev/wordpress:7.4 7.4
+docker build -t kooldev/wordpress:7.4-nginx 7.4-nginx
--- template/Dockerfile.blade.php
+++ template/Dockerfile.blade.php
@@ -1,11 +1,13 @@
 FROM {{ $from }}
 
 ENV PHP_MEMORY_LIMIT=256M \
+    PHP_MAX_INPUT_VARS=1000 \
     PHP_UPLOAD_MAX_FILESIZE=50M \
     PHP_POST_MAX_SIZE=50M \
     PHP_MAX_EXECUTION_TIME=30
 
 RUN adduser -D -u 1337 kool \
+    && addgroup kool www-data \
     # dockerize
     && curl -L https://github.com/jwilder/dockerize/releases/download/v0.6.1/dockerize-alpine-linux-amd64-v0.6.1.tar.gz | tar xz \
     && mv dockerize /usr/local/bin/dockerize \
@@ -14,11 +16,12 @@ RUN adduser -D -u 1337 kool \
     && pecl install redis \
     && docker-php-ext-enable redis \
     && cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini \
+    && sed -i "s/user\ \=.*/user\ \= kool/g" /usr/local/etc/php-fpm.d/www.conf \
     # cleanup
     && apk del .build-deps \
     && rm -rf /var/cache/apk/* /tmp/*
 
 COPY kool.ini /kool/kool.tmpl
 
-ENTRYPOINT [ "dockerize", "-template", "/kool/kool.tmpl:/usr/local/etc/php/conf.d/kool.ini", "docker-entrypoint.sh" ]
+ENTRYPOINT [ "dockerize", "-template", "/kool/kool.tmpl:/usr/local/etc/php/conf.d/zz-kool.ini", "docker-entrypoint.sh" ]
 CMD [ "php-fpm" ]
