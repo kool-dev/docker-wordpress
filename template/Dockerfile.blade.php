0e12ea5b4294066422a687d4f3e49bad89870183
Daniel Polito <danielbpolito@gmail.com>
2021-09-16 14:24:08 -0300

    Remove MySQLi as it's now installed on base image

--- template/Dockerfile.blade.php
+++ template/Dockerfile.blade.php
@@ -6,14 +6,6 @@ FROM {{ $from }}
 ENV NGINX_ROOT=/app
 @endif
 
-RUN echo "Building Deps" \
-    # build-deps
-    && apk add --no-cache --virtual .build-deps $PHPIZE_DEPS \
-    && docker-php-ext-install -j$(nproc) mysqli \
-    # cleanup
-    && apk del .build-deps \
-    && rm -rf /var/cache/apk/* /tmp/*
-
 COPY --from=wordpress-cli /usr/local/bin/wp /usr/local/bin/wp
 COPY --from=wordpress --chown=kool:kool /usr/src/wordpress /kool/wordpress
 COPY --from=wordpress --chown=kool:kool /var/www/html/wp-content /app/wp-content
